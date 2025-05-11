586dd57dee6377a40caec8caac8554d2234186a8
vantran195 <tran1951999@gmail.com>
2025-05-11 23:11:49 +0700

    loginV1
    tập sử dụng react hook form

--- project88/src/App.js
+++ project88/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import LoginPage from './pages/LoginPage';
 function App() {
   return (
     <div>
-      hello
+      <LoginPage/>
     </div>
   );
 }
