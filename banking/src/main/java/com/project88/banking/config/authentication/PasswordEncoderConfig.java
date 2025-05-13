1e9ba3fa9a342f29ea478adf6a908350cfe6b052
phantrongvinh <1phantrongvinh98@gmail.com>
2025-05-13 22:19:22 +0700

    commit file

--- banking/src/main/java/com/project88/banking/config/authentication/PasswordEncoderConfig.java
+++ banking/src/main/java/com/project88/banking/config/authentication/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package com.project88.banking.config;
+package com.project88.banking.config.authentication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
