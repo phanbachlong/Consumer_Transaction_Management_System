66a382dd812b7c18a93b9f1b8bf576e4445ff378
vantran195 <tran1951999@gmail.com>
2025-05-14 20:59:32 +0700

    chỉnh sửa button "more action"

--- project88/src/components/Header.js
+++ project88/src/components/Header.js
@@ -9,7 +9,9 @@ const Header = ({ toggleTheme, currentTheme }) => (
         {currentTheme === "light" ? "Light/Dark" : "Dark/Light"}
       </button>
       <button className="px-4 py-2 bg-gray-100 text-gray-700 rounded hover:bg-gray-200">
-        EN/VN
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
       </button>
     </div>
   </header>
