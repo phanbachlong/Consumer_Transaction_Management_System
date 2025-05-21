db2ebdf978707b11a80a033b67309abaed3e701b
phantrongvinh <1phantrongvinh98@gmail.com>
2025-05-22 00:09:12 +0700

    1

--- project88/src/features/home/UserContent.js
+++ project88/src/features/home/UserContent.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import TransferForm from "./Tranfer";
-import Deposite from "./Deposite";
-import Redeem from "./Redeem";
 import PaymentModal from "../../components/PaymentModal";
 import DepositModal from "../../components/DepositModal";
+import Transaction from "../user/Transaction";
 
 const HomeContent = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -59,14 +57,10 @@ const HomeContent = () => {
                 <div className="text-gray-500 ">Tiết kiệm</div>
                 <div className="text-2xl font-bold">1.000.000.000 VND</div>
                 <div className="flex justify-between space-x-4 mt-2">
-                  <button className="w-1/2 h-12 px-4 py-2 bg-red-100 text-red-600 rounded hover:bg-red-200 overflow-hidden whitespace-nowrap text-ellipsis"
-                    onClick={() => setShowDeposite(true)}
-                  >
+                  <button className="w-1/2 h-12 px-4 py-2 bg-red-100 text-red-600 rounded hover:bg-red-200 overflow-hidden whitespace-nowrap text-ellipsis">
                     Gửi
                   </button>
-                  <button className=" w-1/2 h-12 px-4 py-2 bg-red-100 text-red-600 rounded hover:bg-red-200 overflow-hidden whitespace-nowrap text-ellipsis"
-                    onClick={() => setShowRedeem(true)}
-                  >
+                  <button className=" w-1/2 h-12 px-4 py-2 bg-red-100 text-red-600 rounded hover:bg-red-200 overflow-hidden whitespace-nowrap text-ellipsis">
                     Rút
                   </button>
                 </div>
@@ -74,40 +68,10 @@ const HomeContent = () => {
             </div>
           </div>
         </div>
-
         {/* Transaction History */}
         <div className="bg-white p-6 rounded shadow">
           <h3 className="text-lg font-bold mb-4">Lịch sử giao dịch</h3>
-          {/* <table className="w-full border-collapse border border-gray-300">
-            <thead>
-              <tr className="bg-gray-100">
-                <th className="border border-gray-300 px-4 py-2">Ngày</th>
-                <th className="border border-gray-300 px-4 py-2">Loại</th>
-                <th className="border border-gray-300 px-4 py-2">Nội dung</th>
-                <th className="border border-gray-300 px-4 py-2">Phí</th>
-                <th className="border border-gray-300 px-4 py-2">Số dư</th>
-              </tr>
-            </thead>
-            <tbody>
-              {[...Array(6)].map((_, i) => (
-                <tr key={i} className="text-center">
-                  <td className="border border-gray-300 px-4 py-2">04/05/2025</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    {i % 2 === 0 ? "CK" : "HD"}
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    {i % 2 === 0
-                      ? "Chuyển khoản đến 123456789"
-                      : "Thanh toán HD 987654321"}
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">-1.000.000 VND</td>
-                  <td className="border border-gray-300 px-4 py-2">900.000.000 VND</td>
-                </tr>
-              ))}
-            </tbody>
-          </table> */}
           <Transaction></Transaction>
-
           {/* Page */}
           <div className="text-right mt-4">
             <button className="px-4 py-2 bg-gray-100 text-gray-700 rounded hover:bg-gray-200">
@@ -129,25 +93,25 @@ const HomeContent = () => {
           </div>
         </div>
       </div>
-
       {/* Thanh toán hóa đơn */}
       <div className="w-full lg:w-1/5 space-y-4">
         {[...Array(3)].map((_, i) => (
           <div className="p-4 bg-white shadow rounded" key={i}>
             <div className="text-lg font-bold">Hóa đơn điện</div>
             <div className="text-gray-500 mt-2">
-              Mã HD: 987654321
+              Mã HD: {i === 0 ? "98765321" : i === 1 ? "98765421" : "98765421"}
               <br />
               Số tiền cần thanh toán: 1.000.000 VND
             </div>
-            <button className="mt-4 w-full px-4 py-2 bg-red-100 text-red-600 rounded hover:bg-red-200"
-              onClick={() => setShowPayment(true)}>
+            <button
+              className="mt-4 w-full px-4 py-2 bg-red-100 text-red-600 rounded hover:bg-red-200"
+              onClick={() => handlePaymentClick(i === 0 ? "98765321" : i === 1 ? "98765421" : "98765421")}
+            >
               Thanh Toán
             </button>
           </div>
         ))}
       </div>
-
       {/* Payment Modal */}
       <PaymentModal
         isOpen={isModalOpen}
@@ -164,5 +128,4 @@ const HomeContent = () => {
     </main>
   );
 };
-
-export default UserContent;
+export default HomeContent;
