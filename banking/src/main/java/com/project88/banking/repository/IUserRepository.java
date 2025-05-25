d16be51eec3cb75cb845ee5448c9b1f6eebdd516
vantran195 <tran1951999@gmail.com>
2025-05-25 22:14:28 +0700

    sua lai chuyen khoan

--- banking/src/main/java/com/project88/banking/repository/IUserRepository.java
+++ banking/src/main/java/com/project88/banking/repository/IUserRepository.java
@@ -13,6 +13,7 @@ public interface IUserRepository extends JpaRepository<User, Short> {
 
 	@Query("SELECT c.user FROM CardNumber c WHERE c.cardNumber = :cardNumber")
 	User findUserByCardNumber(@Param("cardNumber") Integer cardNumber);
+	
     User findByCccd(String cccd);
 
     @Transactional
